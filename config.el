d6daf603bb526b0270988feb37ba5f5ec4d95261
Book-reader <89329785+Book-reader@users.noreply.github.com>
2023-08-03 17:18:59 +0800

    initial commit (some errors on startup)

---
+++ config.el
@@ -0,0 +1,18 @@
+
+
+(package! org-bullets)
+(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-one)
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+;;(org-babel-load-file
