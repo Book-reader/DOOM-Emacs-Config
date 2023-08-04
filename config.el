63bdf5247381a9ac0617f9634a894e8a8e79323a
Samuel Talbot <samueltalbotnz@gmail.com>
2023-08-04 17:32:03 +1200

    Sorted some code

--- config.el
+++ config.el
@@ -1,23 +1,14 @@
+(setq doom-theme 'doom-one)
+
+(setq display-line-numbers-type t)
+
 (use-package! org-auto-tangle
   :defer t
   :hook (org-mode . org-auto-tangle-mode)
   :config
   (setq org-auto-tangle-default nil))
-
 (add-hook 'org-mode-hook 'org-auto-tangle-mode)
 
 (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
 
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
-
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
-
-;;(org-babel-load-file
